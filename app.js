f186ba85bb4178daeac83bcf743f22d1e993a87a
ArtemisHH <98184967+ArtemisHH@users.noreply.github.com>
2023-04-09 02:05:10 -0700

    a8 REST sp23

--- app.js
+++ app.js
@@ -5,11 +5,20 @@ import cors from 'cors';
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+
+/*import TuiterController from "./tuiter/tuiter-controller.js";
+import MathController from "./math/math-controller.js";*/
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 /*app.get('/hello', (req, res) => {res.send('Life is good!')})
 app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})*/
+
+/*
+MathController(app);
+TuiterController(app);
+*/
 
 HelloController(app);
 UserController(app);
